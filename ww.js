5df67c8e881624814417d9f9bcb9ec656a4bfa44
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-05-14 17:05:11 +0300

    Update ww.js
    убрала лишнее

--- ww.js
+++ ww.js
@@ -2,9 +2,9 @@
 let mass = [1, 2, [1.1, 1.2, 1.3], 3];
 let mass1 = [1, 2, 3, [4, [5_1, 5_2]]];
 
-let generateList = function (item, array) {
+generateList(item, array) {
   let ul = document.createElement('ul');
-  item.forEach(item => {
+  item.forEach(function (item) {
     let li = document.createElement('li');
     if (!Array.isArray(item)) {
       li.innerHTML = item;
